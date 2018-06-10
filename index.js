328bcaf3b08e5c2e5bb1fbcf61d63e766e5d2a86
shubham0008 <sharma.shubham0008@gmail.com>
2018-06-10 18:01:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express =require('express');
+var socket = require('socket.io');
+//App Setup
+var app = express();
+var server = app.listen(4000,function () {
+    console.log("Server Working");
+})
+
+//static
+app.use(express.static('public'));
+
+
+//Socket Setup
+var io = socket(server);
+
+io.on('connection',function (socket) {
+    console.log('made socket connection',socket.id);
+
+socket.on('chat',function (data) {
+    io.sockets.emit('chat',data);
+
+});
+socket.on('typing',function (data) {
+    socket.broadcast.emit('typing',data);
+
+});
+
+
+});
