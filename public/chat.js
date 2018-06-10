08bce6efc1be0873f2d9d7a62d6441cef9700d7b
shubham0008 <sharma.shubham0008@gmail.com>
2018-06-10 18:11:41 +0530

    All done

--- public/chat.js
+++ public/chat.js
@@ -31,6 +31,6 @@ socket.on('chat',function (data) {
 })
 
 socket.on('typing',function (data) {
-    feedback.innerHTML='<p><em>'+data+ 'is typing a message</em></p>';
+    feedback.innerHTML='<p><em>'+data+ '  is typing a message........</em></p>';
     
 })
