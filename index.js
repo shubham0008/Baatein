31a6d7113584570a264fe26e1eb029d1933e56f8
shubham0008 <sharma.shubham0008@gmail.com>
2018-06-19 00:48:11 +0530

    Some Login Initiation

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var mongoose = require('mongoose');
 //App Setup
 var numUsers = 0;
 var app = express();
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 4000));
 var server = app.listen(app.get('port'),function () {
     console.log("Server Working");
-})
+});
 
 //static
 app.use(express.static('public'));
@@ -22,16 +22,16 @@ io.on('connection',function (socket) {
     console.log('made socket connection',socket.id);
 
 
- socket.on('add user',function (username) {
-     if (addedUser) return;
-     socket.username=username;
-     ++numUsers;
-     addedUser = true;
-
-     socket.emit('login',{numUsers :numUsers});
-
-
- })
+    // socket.on('add user',function (username) {
+    //     if (addedUser) return;
+    //     socket.username=username;
+    //     ++numUsers;
+    //     addedUser = true;
+    //
+    //     socket.emit('login',{numUsers :numUsers});
+    //
+    //
+    // })
 
 socket.on('chat',function (data) {
     io.sockets.emit('chat',data);
